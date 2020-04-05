035220e4945673d6c08930c610149085b4918d82
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-04-05 09:06:41 +0100

    exclude tests from built package

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-lazy-services",
-    version="0.0.2",
+    version="0.0.3",
     author="Gordon Wrigley",
     author_email="gordon.wrigley@gmail.com",
     description="A helper for switching between test and production versions of a service module",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tolomea/django-lazy-services",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
